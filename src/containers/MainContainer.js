d58bad0030073a50b1e30078b8af6e6330113c84
github username <github email address>
2019-11-25 11:07:36 -0500

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -4,28 +4,62 @@ import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
-
-  render() {
-    return (
-      <div>
-        <SearchBar/>
-
-          <div className="row">
-            <div className="col-8">
-
-              <StockContainer/>
-
-            </div>
-            <div className="col-4">
-
-              <PortfolioContainer/>
-
-            </div>
-          </div>
-      </div>
-    );
+  state = {
+    stocks: [],
+    isAlphabetical: true,
+    selectValue: 'Tech',
+    myStocks: []
   }
 
+  changeIsAlphabetical = () => this.setState({isAlphabetical: !this.state.isAlphabetical} )
+  changeSelectValue = (event) => this.setState({selectValue: event.target.value}) 
+  
+  addToMyStocks = (stock) => {
+    const { myStocks } = this.state
+    if (!myStocks.includes(stock)) this.setState({myStocks: [...myStocks, stock]})
+  }
+
+  removeToMyStocks = (stockToRemove) => {
+    const {myStocks} = this.state
+    this.setState({
+      myStocks: myStocks.filter(stock => stock.id !== stockToRemove.id)
+    })
+  }
+
+  componentDidMount () {
+    fetch('http://localhost:3000/stocks') //eslint-disable-line
+      .then(response => response.json())
+      .then(stocks => this.setState({stocks}))
+  }
+
+  render () {
+    const { stocks, selectValue, isAlphabetical, myStocks } = this.state
+    const filteredStocks = stocks.filter(stock => stock.type === selectValue )
+  
+    if (isAlphabetical) filteredStocks.sort((s1, s2) => s1.name.localeCompare(s2.name))
+    else filteredStocks.sort((s1, s2) => s1.price - s2.price)
+    return (
+      <div>
+        <SearchBar
+          isAlphabetical={this.state.isAlphabetical}
+          onChangeChecked={this.changeIsAlphabetical}
+          defaultSelectValue={this.state.selectValue}
+          changeSelectValue={this.changeSelectValue}
+        />
+
+        <div className='row'>
+          <div className='col-8'>
+
+            <StockContainer addStock={this.addToMyStocks} stocks={filteredStocks}/>
+
+          </div>
+          <div className='col-4'>
+            <PortfolioContainer removeStock={this.removeToMyStocks} stocks={myStocks}/>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default MainContainer;
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Stock from '../components/Stock'
 
 class PortfolioContainer extends Component {
-
-  render() {
+  render () {
+    const { stocks, removeStock } = this.props
     return (
       <div>
         <h2>My Portfolio</h2>
-          {
-            //render your portfolio stocks here
-          }
+        {
+          stocks.map(stock => <Stock removeStock={removeStock} stock={stock} key={stock.id} />)
+        }
       </div>
-    );
+    )
   }
-
 }
 
-export default PortfolioContainer;
+export default PortfolioContainer
