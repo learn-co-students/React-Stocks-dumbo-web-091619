d948a7124784add63687cf874008bcbaa2311e6e
Noam Sauer-Utley <noamsauerutley@gmail.com>
2019-11-27 13:50:17 -0500

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,20 +5,95 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+  state = {
+    stocks: [],
+    stocksDisplay: [],
+    portfolio: [],
+  }
+
+  componentDidMount = async () => {
+    let rawStocks = await fetch('http://localhost:3000/stocks')
+    let stocks = await rawStocks.json()
+    this.setState({
+      stocks,
+      stocksDisplay: stocks
+    })
+    console.log(this.state.stocksDisplay)
+  }
+
+  addToPortfolio = (selectedStock) => {
+    this.setState({
+      portfolio: [...this.state.portfolio, selectedStock]
+    })
+
+  }
+
+  removeFromPortfolio = (selectedStock) => {
+    let currentPortfolio = this.state.portfolio.filter(stock => {
+      return stock.id !== selectedStock.id
+    })
+    this.setState({
+      portfolio: currentPortfolio
+    }) 
+  }
+
+  alphabetizeStocks = () => {
+    let alphabetizedStocks = [...this.state.stocks].sort(
+      (a, b) => {
+        if(a.name < b.name) { return -1 }
+        if(a.name > b.name) { return 1 }
+        return 0
+      }
+    )
+    this.setState({
+      stocksDisplay: alphabetizedStocks
+    })
+  }
+
+  priceStocks = () => {
+    let pricedStocks = [...this.state.stocks].sort(
+      (a, b) => {
+        if(a.price < b.price) { return -1 }
+        if(a.price > b.price) { return 1 }
+        return 0
+      }
+    )
+    this.setState({
+      stocksDisplay: pricedStocks
+    })
+  }
+
+  resetStocks = () => {
+    this.setState({
+      stocksDisplay: this.state.stocks
+    })
+    console.log(this.state.stocksDisplay)
+  }
+
+  filterStocks = (filterValue) => {
+    let filteredStocks = this.state.stocks.filter(stock => {
+        return stock.type === filterValue
+      })
+      this.setState({
+      stocksDisplay: filteredStocks
+    })
+  }
+ 
+
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar alphabetizeStocks={this.alphabetizeStocks} priceStocks={this.priceStocks} resetStocks={this.resetStocks} filterStocks={this.filterStocks}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocksDisplay={this.state.stocksDisplay} addToPortfolio={this.addToPortfolio}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer portfolio={this.state.portfolio} removeFromPortfolio={this.removeFromPortfolio}/>
 
             </div>
           </div>
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Stock from '../components/Stock'
+import PortfolioStock from '../components/PortfolioStock'
 
 class PortfolioContainer extends Component {
 
@@ -8,7 +8,9 @@ class PortfolioContainer extends Component {
       <div>
         <h2>My Portfolio</h2>
           {
-            //render your portfolio stocks here
+           this.props.portfolio.map(stock => {
+             return <PortfolioStock stock={stock} removeFromPortfolio={this.props.removeFromPortfolio}/>
+           })
           }
       </div>
     );
